43cdcdf72070f679eb54d44c38839b28c1ce1387
Tony Fischetti <tony.fischetti@gmail.com>
2020-04-27 14:29:04 -0400

    had to enable tapping no that I use libinput for all

--- startup.sh
+++ startup.sh
@@ -6,6 +6,7 @@ THEHOST=`hostname`
 
 if [ $THEHOST == "qonos" ] ; then
     xinput --set-int-prop 13 "libinput Natural Scrolling Enabled" 8 1
+    xinput --set-int-prop 13 "libinput Tapping Enabled" 8 1
 fi
 
 sh ~/starlight/start-starlight.sh
