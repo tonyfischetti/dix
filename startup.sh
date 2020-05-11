922546009d422ee5136160e0f41b75eba6a25c0e
Tony Fischetti <tony.fischetti@gmail.com>
2020-05-11 12:07:35 -0400

    now have starlight handled by systemd

--- startup.sh
+++ startup.sh
@@ -2,12 +2,16 @@
 
 # add this to the xfce session autostart executing "sh /home/tony/.dix/startup.sh"
 
-sh ~/starlight/start-starlight.sh
+# STARTLIGHT IS NOW MANAGED BY SYSTEMD
+# sh ~/starlight/start-starlight.sh
 
+touch "/home/tony/Desktop/opened"
 THEHOST=`hostname`
 
 if [ $THEHOST == "qonos" ] ; then
     xinput --set-int-prop 13 "libinput Natural Scrolling Enabled" 8 1
     xinput --set-int-prop 13 "libinput Tapping Enabled" 8 1
 fi
+
+exit 0
 
