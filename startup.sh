cc03d89f9756d54c58ab34a3ee71539cd4460c41
Tony Fischetti <tony.fischetti@gmail.com>
2020-05-12 14:21:07 -0400

    adsa

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# set -euxo pipefail
+set -euxo pipefail
 
 # add this to the xfce session autostart executing "sh /home/tony/.dix/startup.sh"
 
@@ -8,11 +8,13 @@
 # sh ~/starlight/start-starlight.sh
 
 
+echo "setting custom xmodmap"
 /usr/bin/xmodmap /home/tony/.Xmodmap
 
 THEHOST=`hostname`
 
 if [ $THEHOST == "qonos" ] ; then
+    echo "setting up qonos-specific touchpad things"
     xinput --set-int-prop 13 "libinput Natural Scrolling Enabled" 8 1
     xinput --set-int-prop 13 "libinput Tapping Enabled" 8 1
 fi
