f1cd009df37ebcf800bf26c566d3cf1922414443
Tony Fischetti <tony.fischetti@gmail.com>
2020-11-09 09:25:52 -0500

    startup script is now more robust and more chatty

--- startup.sh
+++ startup.sh
@@ -2,21 +2,20 @@
 
 set -euxo pipefail
 
-# add this to the xfce session autostart executing "sh /home/tony/.dix/startup.sh"
 
-# STARTLIGHT IS NOW MANAGED BY SYSTEMD
-# sh ~/starlight/start-starlight.sh
-
-
-echo "setting custom xmodmap"
+echo "setting custom xmodmap" | systemd-cat -t miscellaneous
 /usr/bin/xmodmap /home/tony/.Xmodmap
+echo "successful" | systemd-cat -t miscellaneous
 
 THEHOST=`hostname`
 
 if [ $THEHOST == "qonos" ] ; then
-    echo "setting up qonos-specific touchpad things"
-    xinput --set-int-prop 13 "libinput Natural Scrolling Enabled" 8 1
-    xinput --set-int-prop 13 "libinput Tapping Enabled" 8 1
+    echo "setting up qonos-specific touchpad things" | systemd-cat -t miscellaneous
+    WHICHONE=`xinput | egrep 'ELAN.+Touchpad' | perl -pe 's/.+id=(\d+).+/$1/'`
+    echo "choosing xinput $WHICHONE" | systemd-cat -t miscellaneous
+    xinput --set-int-prop $WHICHONE "libinput Natural Scrolling Enabled" 8 1
+    xinput --set-int-prop $WHICHONE "libinput Tapping Enabled" 8 1
+    echo "successful" | systemd-cat -t miscellaneous
 fi
 
 
