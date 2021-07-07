7563f1cdfe7550c430b4d93f3b3bec9e1e9de558
Tony Fischetti <tony.fischetti@gmail.com>
2021-07-07 13:54:43 -0400

    reverse scrolling works fine in xfce4.16 now...
    no need for the stuff qonos stuff in the systemd target

--- startup.sh
+++ startup.sh
@@ -7,15 +7,15 @@ echo "setting custom xmodmap" | systemd-cat -t miscellaneous
 /usr/bin/xmodmap /home/tony/.Xmodmap
 echo "successful" | systemd-cat -t miscellaneous
 
-THEHOST=`hostname`
-
-if [ $THEHOST == "qonos" ] ; then
-    echo "setting up qonos-specific touchpad things" | systemd-cat -t miscellaneous
-    WHICHONE=`xinput | egrep 'ELAN.+Touchpad' | perl -pe 's/.+id=(\d+).+/$1/'`
-    echo "choosing xinput $WHICHONE" | systemd-cat -t miscellaneous
-    xinput --set-int-prop $WHICHONE "libinput Natural Scrolling Enabled" 8 1
-    xinput --set-int-prop $WHICHONE "libinput Tapping Enabled" 8 1
-    echo "successful" | systemd-cat -t miscellaneous
-fi
+# THEHOST=`hostname`
+#
+# if [ $THEHOST == "qonos" ] ; then
+#     echo "setting up qonos-specific touchpad things" | systemd-cat -t miscellaneous
+#     WHICHONE=`xinput | egrep 'ELAN.+Touchpad' | perl -pe 's/.+id=(\d+).+/$1/'`
+#     echo "choosing xinput $WHICHONE" | systemd-cat -t miscellaneous
+#     xinput --set-int-prop $WHICHONE "libinput Natural Scrolling Enabled" 8 1
+#     xinput --set-int-prop $WHICHONE "libinput Tapping Enabled" 8 1
+#     echo "successful" | systemd-cat -t miscellaneous
+# fi
 
 
